bc3041e27fa9711af13cf316612cddc37b16d8de
Märt Maadla <mart.maadla@reaalprojekt.ee>
2022-02-13 10:34:28 +0200

    Hello Word prorgamm

---
+++ HelloWord/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace HelloWord
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Kes sa oled, mis on sinu nimi?");
+			string Username;
+			Username = Console.ReadLine();
+
+			Console.WriteLine("Tere, "  + Username);
+
+			Console.Read();
+		}
+	}
+}
+Märt
